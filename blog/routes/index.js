7798692b3a53bbc6f0dc626c32ed91c03b431290
Edgar <lipeizhao22@gmail.com>
2017-01-03 16:32:01 +0800

    add basic routes

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function(app) {
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: '主页'});
+  });
+  
+  app.get('/reg', function(req, res) {
+    res.render('reg', { title: '注册'});
+  });
+  // app.post('/reg', function(req, res) {
+  //   var name = req.body.name,
+  //     password = req.body.password,
+  //     password_re = req.body['password-repeat'];
+  //  
+  //   if (password_re != password) {
+  //     req.flash('error', '两次输入的密码不一致！');
+  //     return res.redirect('/reg');
+  //   }
+  //  
+  //   var md5 = crypto.createHash('md5'),
+  //     password = md5.update(req.body.password).digest('hex');
+  //   var newUser = new User({
+  //     name: req.body.name,
+  //     password: req.body.password,
+  //     email: req.body.email
+  //   });
+  //  
+  //   User.get(newUser, function(err, user) {
+  //     if (err) {
+  //       req.flash('error', err);
+  //       return res.redirect('/');
+  //     }
+  //     if(user) {
+  //       req.flash('error', '用户已经存在');
+  //       return res.redirect('/reg');
+  //     }
+  //     newUser.save(function (err, user) {
+  //       if (err) {
+  //         req.flash('error', err);
+  //         return res.redirect('/reg');
+  //       }
+  //       req.sessiong.user = user;
+  //       req.flash('success', '注册成功！');
+  //       res.redirect('/');
+  //     });
+  //   });
+  // });
+  
+  app.get('/login', function(req, res) {
+    res.render('login', { title: '登陆'});
+  });
+  app.post('/login', function(req, res) {
+  });
+  
+  app.get('/post', function(req, res) {
+    res.render('post', { title: '发表'});
+  });
+  app.post('/post', function(req, res) {
+  });
+}
